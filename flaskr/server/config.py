dc2948228fe3ae490ce92ad1e2a15e75cf61018f
Vasyl Turko <vturko@lvivsoft.com>
2017-04-27 11:32:07 +0300

    after rem gitignore config

--- flaskr/server/config.py
+++
@@ -1,6 +0,0 @@
-
-class DevelopmentConfig(object):
-    """Development configuration."""
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:chokolatka@localhost:3306/td2'
-
